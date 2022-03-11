dd31a234b258286752a193539e1ff05afdbb1970
Kenneth Leung <kennethleung.ty@gmail.com>
2022-03-11 23:37:15 +0800

    Initial commit

---
+++ src/anomaly_detection_pipeline_kedro/__init__.py
@@ -0,0 +1,4 @@
+"""Anomaly Detection Pipeline (Kedro)
+"""
+
+__version__ = "0.1"
---
+++ src/tests/test_run.py
@@ -0,0 +1,29 @@
+"""
+This module contains an example test.
+
+Tests should be placed in ``src/tests``, in modules that mirror your
+project's structure, and in files named test_*.py. They are simply functions
+named ``test_*`` which test a unit of logic.
+
+To run the tests, run ``kedro test`` from the project root directory.
+"""
+
+from pathlib import Path
+
+import pytest
+from kedro.framework.context import KedroContext
+
+
+@pytest.fixture
+def project_context():
+    return KedroContext(
+        package_name="anomaly_detection_pipeline_kedro", project_path=Path.cwd()
+    )
+
+
+# The tests below are here for the demonstration purpose
+# and should be replaced with the ones testing the project
+# functionality
+class TestProjectContext:
+    def test_package_name(self, project_context):
+        assert project_context.package_name == "anomaly_detection_pipeline_kedro"
